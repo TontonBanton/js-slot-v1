b51c56533be00caa026b87a00c9f56b16740337f
TontonBanton <pancla13ulas@gmail.com>
2024-12-06 17:17:48 +0800

    Animation running need tuning

--- script.js
+++ script.js
@@ -20,41 +20,59 @@ let slot = {
       slot.animation(index);
     }
   },
-    init: function() {                                              // slot initialization of reel values
-      stopReelFlag[0] = stopReelFlag[1] = stopReelFlag[2] = false;  // stopReelFlag = [false, false, false]
-      reelCounts[0] = reelCounts[1] = reelCounts[2] = 0;               // reelCount = [0, 0, 0]
+    init: function() {                                                  // slot initialization of reel values
+      stopReelFlag[0] = stopReelFlag[1] = stopReelFlag[2] = false;      // stopReelFlag = [false, false, false]
+      reelCounts[0] = reelCounts[1] = reelCounts[2] = 0;                // reelCount = [0, 0, 0]
       console.log("Initialization complete.");
     },
-    animation: function(index) {                                    // slot animation
-    console.log("animate for index ", index);
-     },
 
   stop: function(i) {
     stopReelFlag[i] = true;
     console.log("stop ", stopReelFlag)
-    if (stopReelFlag[0] && stopReelFlag[1] && stopReelFlag[2]) {      // stopReelFlag = [true, true, true] - all 3 is clicked
-      start_btn.removeAttribute("disabled");                          // Enable the start button whe all reels are stop
+    if (stopReelFlag[0] && stopReelFlag[1] && stopReelFlag[2]) {      // stopReelFlag = [true, true, true] - all 3buttons clicked
+      start_btn.removeAttribute("disabled");                          // Enable the start button again when all reels are stop
       alert('enable start')
     }
   },
 
+  //Initial position of the reel in the frame
   resetLocationInfo: function() {
     slotFrameHeight = slot_screen.offsetHeight;           // Log the frame height (480)
-    slotReeltemHeight = reel[0].offsetHeight;             // Log a single reel's height (244)
-    slotReelsHeight = reels[0].offsetHeight;              // Log the reels' height (2451) - (244x10 plus margins/border)
+    slotReeltemHeight = reel[0].offsetHeight;             // Log a single reel's(image) height (244)
+    slotReelsHeight = reels[0].offsetHeight;              // Log the all (10) reels height (2451) - (244x10 plus margins/border)
 
-    slotReelStartHeight = -slotReelsHeight;               // Move the whole reels directly above the 0 axis (-2145)
-    slotReelStartHeight += slotFrameHeight - (slotFrameHeight / 2) + ( slotReeltemHeight * 1.5);
-    console.log("Final slotReelStartHeight (after adjustment):", slotReelStartHeight);
-    alert('isa isaha')
+    slotReelStartHeight = -slotReelsHeight;                               // Move the whole reel above the frame (0axis)
+    slotReelStartHeight += slotFrameHeight - (slotFrameHeight / 2)        // Move the reel bottom on the middle of the frame (240/2)
+    slotReelStartHeight += slotFrameHeight + ( slotReeltemHeight * 1.5);  // Adjust the reel to move down 1.5 percent overflowing half of the bottom
+
+    //Loop to adjust all the reel[0,1,2] to start on the last adjustment position
     for (let i = 0; i < reels.length; i++) {
         reels[i].style.top = String(slotReelStartHeight) + "px";
         console.log(`Reel ${i} style.top set to:`, reels[i].style.top);
     }
+  },
+
+  animation: function(index){
+    console.log("animate for index ", index);
+    if (reelCounts[index] >= 8 ) { reelCounts[index] = 0 }
+
+    $(".reels").eq(index).animate(
+      {
+        "top": slotReelStartHeight + (reelCounts[index] * slotReeltemHeight)
+      },
+      {
+        duration: sec,
+        easing: "linear",
+        complete: function(){
+          if(stopReelFlag[index]) { return }
+          reelCounts[index]++
+          slot.animation(index)
+        }
+      }
+    )
+  }
+
 }
-
-};
-
 
 
 //START BUTTON------------------------------------------------------------------------------------------------------------
